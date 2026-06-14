a7bdc077f205ab539358b7f1c64768571084cf6b
Aleks <alekeci@icloud.com>
2026-06-14 12:58:56 +0200

    Add ads.txt redirect for Ezoic

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/19390/worldwiderecipes.app',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
